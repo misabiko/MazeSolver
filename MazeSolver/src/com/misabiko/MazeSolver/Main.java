f78febe0b0515579728b70fab077a72b1bee1fec
misabiko <misabiko@gmail.com>
2014-09-29 07:54:13 -0400

    Putting the "Wall Following" algorithm in its own class.

--- MazeSolver/src/com/misabiko/MazeSolver/Main.java
+++ MazeSolver/src/com/misabiko/MazeSolver/Main.java
@@ -12,9 +12,10 @@ import javax.imageio.ImageIO;
 public class Main extends Applet{;
 
 	private static final long serialVersionUID = 1L;
-	private static BufferedImage maze;
-	private int startX, startY, endX, endY, currX, currY, wall, hall, path, deadEnd;
+	private static BufferedImage srcMaze, maze;
+	private int startX, startY, endX, endY, currX, currY, wall, hall, start, end, path, deadEnd;
 	private int deadEndIndex = 0;
+	private int tileSize = 1;
 	private enum direction { UP, RIGHT, DOWN, LEFT }
 	private direction currD;
 	
@@ -22,19 +23,29 @@ public class Main extends Applet{;
 		
 //		Set the maze's BufferedImage
 		try {
-			maze = ImageIO.read(new File("bigmaze.png"));
+			srcMaze = ImageIO.read(new File("maze_image.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+		maze = new BufferedImage(srcMaze.getWidth(),srcMaze.getHeight(),BufferedImage.TYPE_3BYTE_BGR);
+		maze.createGraphics().drawImage(srcMaze,0,0,null);
+		
+		wall = Color.BLACK.getRGB();
+		hall = Color.WHITE.getRGB();
+		start = Color.BLUE.getRGB();
+		end = Color.GREEN.getRGB();
+		path = Color.RED.getRGB();
+		deadEnd = Color.CYAN.getRGB();
+		
 //		Find the start point (blue) and end point (green)
 		for (int y = 0; y < maze.getHeight(); y++) {
 			for (int x = 0; x < maze.getWidth(); x++) {
-				if (maze.getRGB(x, y) == Color.BLUE.getRGB()) {
+				if (maze.getRGB(x, y) == start) {
 					startX = x;
 					startY = y;
 					System.out.println("Start is at "+startX+" "+startY);
-				}else if (maze.getRGB(x, y) == Color.GREEN.getRGB()) {
+				}else if (maze.getRGB(x, y) == end) {
 					endX = x;
 					endY = y;
 					System.out.println("End is at "+endX+" "+endY);
@@ -42,13 +53,10 @@ public class Main extends Applet{;
 			}
 		}
 		
+//		findOpening();
+		
 		currX = startX;
 		currY = startY;
-		
-		wall = Color.BLACK.getRGB();
-		hall = Color.WHITE.getRGB();
-		path = Color.RED.getRGB();
-		deadEnd = Color.CYAN.getRGB();
 		
 //		Setting the start direction, start can't be in corners
 		if (startY == 0)
@@ -64,21 +72,21 @@ public class Main extends Applet{;
 	
 	public void start() {
 		int i = 1;
-		while (i < 10000) {
+		while (i<1000000) {
 //			System.out.println("Step #"+i+": Facing "+currD.toString().toLowerCase()+"."+getCurrPos()+" Dead End Index is at "+deadEndIndex+".");
-			if (check() == Color.GREEN.getRGB()) {
+			if (check() == end) {
 				step();
 				System.out.println("Solution has been found in "+i+" steps.");
 				return;
 			}else if (check() == hall) {
 				step();
 			}else if (deadEndIndex >= 4) {
-				if (check() == wall || check() == deadEnd || check() == Color.BLUE.getRGB()) {
+				if (check() == wall || check() == deadEnd || check() == start) {
 					nextDirection();
 				}else {
 					step();
 				}
-			}else if (check() == wall || check() == path || check() == deadEnd || check() == Color.BLUE.getRGB()) {
+			}else if (check() == wall || check() == path || check() == deadEnd || check() == start) {
 				nextDirection();
 			}
 			
@@ -130,7 +138,7 @@ public class Main extends Applet{;
 					maze.setRGB(currX, currY, deadEnd);
 					
 					System.out.print("Backtracking up.");
-				}else if (maze.getRGB(currX,currY) == Color.BLUE.getRGB()) {
+				}else if (maze.getRGB(currX,currY) == start) {
 					System.out.print("Going up.");
 				}else {
 					maze.setRGB(currX, currY, path);
@@ -146,7 +154,7 @@ public class Main extends Applet{;
 					maze.setRGB(currX, currY, deadEnd);
 					
 					System.out.print("Backtracking right.");
-				}else if (maze.getRGB(currX,currY) == Color.BLUE.getRGB()) {
+				}else if (maze.getRGB(currX,currY) == start) {
 					System.out.print("Going right.");
 				}else {
 					maze.setRGB(currX, currY, path);
@@ -162,7 +170,7 @@ public class Main extends Applet{;
 					maze.setRGB(currX, currY, deadEnd);
 					
 					System.out.print("Backtracking down.");
-				}else if (maze.getRGB(currX,currY) == Color.BLUE.getRGB()) {
+				}else if (maze.getRGB(currX,currY) == start) {
 					System.out.print("Going down.");
 				}else {
 					maze.setRGB(currX, currY, path);
@@ -178,7 +186,7 @@ public class Main extends Applet{;
 					maze.setRGB(currX, currY, deadEnd);
 					
 					System.out.print("Backtracking left.");
-				}else if (maze.getRGB(currX,currY) == Color.BLUE.getRGB()) {
+				}else if (maze.getRGB(currX,currY) == start) {
 					System.out.print("Going left.");
 				}else {
 					maze.setRGB(currX, currY, path);
@@ -208,6 +216,91 @@ public class Main extends Applet{;
 		return " Now at ("+currX+", "+currY+").";
 	}
 	
+	private void findOpening() {
+		for (int i = 0;i< maze.getWidth();i++) {
+			if (maze.getRGB(i, 0) != wall) {
+				if (startX == 0 && startY == 0) {
+					startX = i;
+					startY = 0;
+				}else if (endX == 0 && endY == 0) {
+					endX = i;
+					endY = 0;
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getWidth();i++) {
+			if (maze.getRGB(i, maze.getHeight()-1) != wall) {
+				if (startX == 0 && startY == 0) {
+					startX = i;
+					startY = maze.getHeight();
+				}else if (endX == 0 && endY == 0) {
+					endX = i;
+					endY = maze.getHeight();
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getHeight();i++) {
+			if (maze.getRGB(0, i) != wall) {
+				if (startX == 0 && startY == 0) {
+					startX = 0;
+					startY = i;
+				}else if (endX == 0 && endY == 0) {
+					endX = 0;
+					endY = i;
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getHeight();i++) {
+			if (maze.getRGB(maze.getWidth()-1, i) != wall) {
+				if (startX == 0 && startY == 0) {
+					startX = maze.getWidth();
+					startY = i;
+				}else if (endX == 0 && endY == 0) {
+					endX = maze.getWidth();
+					endY = i;
+					return;
+				}
+			}
+		}
+	}
+	
+	private void setTileSize() {
+		for (int i = 0;i< maze.getWidth();i++) {
+			if (maze.getRGB(i, 0) != wall) {
+				while (maze.getRGB(i+tileSize, 0) != wall) {
+					tileSize++;
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getWidth();i++) {
+			if (maze.getRGB(i, maze.getHeight()) != wall) {
+				while (maze.getRGB(i+tileSize, maze.getHeight()) != wall) {
+					tileSize++;
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getHeight();i++) {
+			if (maze.getRGB(0, i) != wall) {
+				while (maze.getRGB(0, i+tileSize) != wall) {
+					tileSize++;
+					return;
+				}
+			}
+		}
+		for (int i = 0;i< maze.getHeight();i++) {
+			if (maze.getRGB(maze.getWidth(), i) != wall) {
+				while (maze.getRGB(maze.getWidth(), i+tileSize) != wall) {
+					tileSize++;
+					return;
+				}
+			}
+		}
+	}
 	public void paint(Graphics g) {
 		g.drawImage(maze,0,0,getWidth(),getHeight(),null);
 	}
